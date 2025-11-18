5e011d2f3ea9626b7aba2a69f97b91339c9a4e75
EcoJvr101 <jorgealtamirano5a@gmail.com>
2025-11-18 17:36:28 +0000

    Subimos esta madreee

--- Codigo/#Aqui empezaremos con el codigo del proy.py
+++ Codigo/#Aqui empezaremos con el codigo del proy.py
@@ -1,6 +1,6 @@
 import csv
 import os
-path = r"S:\Progamación\TG\Archivos_TG\Reviews.csv"
+path = r"Archivos_TG\Reviews.csv"
 if not os.path.exists(path):
     print(f"Error: El archivo no existe en {path}")
     exit()
